e0ced01c1d10ff0d08f3651c618f863694fe25ca
Chris Hunter <chunter@urbn.com>
2013-09-03 16:53:34 -0400

    Update the sample settings

--- tests.py
+++ tests.py
@@ -6,17 +6,4 @@ meta = {
 	"gallery": "sample_gallery",	
 }
 
-def test_gallery():
-	gallery = Gallery(test, meta)
-	gallery.generate()
-	pprint.pprint(gallery.__dict__)
-
-def test_module_reflection():
-	import lib.plugins
-	instance = lib.plugins.getPluginInstance("gallery", test)
-	print instance
-
-
-test_module_reflection()
-
-#test_gallery()
+## Need to setup tests correctly here
